ce15ab6ff3d360e8d63a2046bf8a455102b8e959
borshevsky <topicnya@gmail.com>
2020-12-26 16:52:15 +0300

    Feature: email form (#1)

---
+++ notify.php
@@ -0,0 +1,14 @@
+<?php
+    $addr = 'info@mtvc.lt'
+
+    $name = $_POST['name'];
+    $phone = $_POST['tel-number'];
+
+    mail($addr, 'New contact request', 'Name: ' . $name . ', Phone: ' . $phone);
+
+    $user_message = "Thank you, " . $name . ', we will recall you as far as possible';
+
+    echo '<script>alert("' . $user_message . '")</script>';
+    echo '<html><a href="https://mtvc.lt">Go back</a></html>';
+    die();
+?>
